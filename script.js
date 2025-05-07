2d81c873b50595882cf06e8cc60f086eb645a411
piero elguedas <piero2086@gmail.com>
2025-05-07 12:23:14 -0500

    cambios

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const elementosEscalables = document.querySelectorAll('.elemento-escalable');
 
+  elementosEscalables.forEach(elemento => {
+      elemento.addEventListener('mouseover', function() {
+          this.classList.add('hovered');
+      });
 
-  let sidemenu = document.getElementById("sidemenu");
-function openmenu(){
+      elemento.addEventListener('mouseout', function() {
+          this.classList.remove('hovered');
+      });
+  });
+});
+
+let sidemenu = document.getElementById("sidemenu");
+  function openmenu(){
     sidemenu.style.right = "0";
-}
+  }
 function closemenu(){
     sidemenu.style.right = "-200px";
-}
+  }
